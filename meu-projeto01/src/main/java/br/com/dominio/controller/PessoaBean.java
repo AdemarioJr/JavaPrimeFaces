165723e7a7a825d58a27170e470e08bbc3d00644
Ademario Junior <ademariofernandesramos@gmail.com>
2020-07-15 10:54:09 -0300

    Testes realizados para a anotação AplicationScoped

--- meu-projeto01/src/main/java/br/com/dominio/controller/PessoaBean.java
+++ meu-projeto01/src/main/java/br/com/dominio/controller/PessoaBean.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.ApplicationScoped;
+//import javax.enterprise.context.RequestScoped;
 //import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -13,7 +14,8 @@ import br.com.dominio.model.Pessoa;
 
 @Named("bean")
 //@SessionScoped
-@RequestScoped
+//@RequestScoped
+@ApplicationScoped
 public class PessoaBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
