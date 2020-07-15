f6bc88005867370fc63a79aeadbc7a69bf6c85fa
Marivania <Marivania@Marivania-Note>
2020-07-15 10:14:14 -0300

    Adicionando import referente a anotação RequestScoped.

--- meu-projeto01/src/main/java/br/com/dominio/controller/PessoaBean.java
+++ meu-projeto01/src/main/java/br/com/dominio/controller/PessoaBean.java
@@ -4,7 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.RequestScoped;
+//import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
